610b1c816646c9a9d64991467e321c51f27e24fc
jay <minalpatel1418@gmail.com>
2023-09-20 10:35:12 +0530

    changes are done

---
+++ fastapiapp/blog/token.py
@@ -0,0 +1,25 @@
+from datetime import timedelta, datetime
+from jose import JWTError, jwt
+from blog.schemas import TokenData
+
+SECRET_KEY = "2dc29c80771946227ee09569453daf1359855d012695328c05bfff44ded6fce4"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def verify_token(token:str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+        token_data = TokenData(email=email)
+    except JWTError:
+        raise credentials_exception
---
+++ fastapiapp/main.py
@@ -0,0 +1,102 @@
+from fastapi import FastAPI, Depends, status, HTTPException
+from blog import schemas
+from typing import List
+from blog import models
+from blog.database import engine, SessionLocal
+from sqlalchemy.orm import Session, relationship
+from blog.hashing import Hash
+from datetime import timedelta
+from blog.token import ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token
+from blog import oauth
+from fastapi.security import OAuth2PasswordRequestForm
+
+
+
+app = FastAPI()
+
+models.Base.metadata.create_all(engine)
+
+#Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post('/blog', status_code=status.HTTP_201_CREATED, tags=["blogs"])
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body, user_id=1)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+@app.get('/blog', response_model=List[schemas.ShowBlog], tags=["blogs"])
+def all(db: Session = Depends(get_db), get_current_user: schemas.User= Depends(oauth.get_current_user)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+@app.get('/blog/{id}', status_code = status.HTTP_200_OK, response_model=schemas.ShowBlog, tags=["blogs"])
+def show(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if blog is None:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
+                            detail=f'Blog with id {id} is not available')
+    return blog
+
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT, tags=["blogs"])
+def destroy(id,  db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
+                            detail=f'Blog with id {id} is not found')
+    blog.delete(synchronize_session=False)
+    return f'blog is deleted with id {id}.'
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED, tags=["blogs"])
+def update(id, request: schemas.Blog,  db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
+                            detail=f'Blog with id {id} is not found')
+    blog.update({'title':request.title, 'body':request.body})
+    db.commit()
+    return 'updated'
+
+
+@app.post('/user', response_model=schemas.ShowUser, tags=["users"])
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(name=request.name, email=request.email,password=Hash.bcrypt(request.password))
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+# @app.post('/blog')
+# def create(y\title, body):
+#     return {'title':title, 'body':body}
+
+@app.get('/user/{id}', response_model=schemas.ShowUser, tags=["users"])
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
+                            detail=f'User with id {id} is not found')
+    return user
+
+@app.post('/login', status_code=status.HTTP_202_ACCEPTED, tags=["Authentication"])
+def login(request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email == request.username).first()
+    if not user:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
+                            detail=f'Invalid Credentials')
+    if not Hash.verify(user.password, request.password):
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
+                            detail=f'Incorrect password')
+    
+    # access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+    data={"sub": user.email}
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
